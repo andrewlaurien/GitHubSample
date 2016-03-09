5ae7191dc9acadc2bba9912d91a373b530e9c46e
Andrew Laurien R. Socia <socia.andrew@gmail.com>
2016-03-09 10:44:28 +0800

    Hi

--- GithubSample/AppDelegate.swift
+++ GithubSample/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        NSLog("This is me.");
+        NSLog("This is from andrew.");
         return true
     }
 
